8bbb282128985237f8fc58454bb3914634b1ed70
Daryna Holikova <dgolikova0@gmail.com>
2025-07-04 09:29:17 +0100

    chnages

--- themes/portfolio/header.php
+++ themes/portfolio/header.php
@@ -16,23 +16,43 @@
 </head>
 
 <body <?php body_class(); ?>>
-    <?php
-    echo 'HEADER LOADED';
-    ?>
-    <header>
+    <header class="site-header">
         <div class="container">
             <a href="<?php echo home_url(); ?>" class="logo">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="logo">
             </a>
 
+            <button class="burger" id="burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+
+            <nav class="mobile-menu" id="mobileMenu">
+                <button class="close-btn" id="closeMenu">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/close-x.svg" alt="close"
+                        height="20p">
+                </button>
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'header_menu',
+                    'menu_id' => 'mobile_menu',
+                    'container' => false,
+                    'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                ));
+                ?>
+            </nav>
+
+
             <?php
-            wp_nav_menu([
+            wp_nav_menu(array(
                 'theme_location' => 'header_menu',
-                'menu_id' => 'header-menu',
+                'menu_id' => 'header_menu',
                 'container' => 'nav',
                 'container_class' => 'header-menu',
                 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            ]);
-            ?>
+            ))
+                ?>
         </div>
     </header>
